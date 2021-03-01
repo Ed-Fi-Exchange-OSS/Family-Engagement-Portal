60c66e05932ca19ae3bea1bbfa090ea1d5f3a699
Shannon Kerlick <shannon.kerlick@ed-fi.org>
2021-03-01 14:26:53 -0600

    Fix SQL scripts for 3.4-5.1 update
    Change the post install script for new names of SQL files in 5.1 (3.4+) upgrade

--- update-SQLdatabase.ps1
+++ update-SQLdatabase.ps1
@@ -1,8 +1,10 @@
 param(
-    [System.Collections.Hashtable] $EdFiParamTable = @{ Ods="Server=.; Database=EdFi_Ods_Demo_v320; Trusted_Connection=True;" }
+    [System.Collections.Hashtable] $EdFiParamTable = @{ Ods="Server=.; Database=EdFi_Ods_Demo_v340; Trusted_Connection=True;"; SolutionPath="C:\Ed-Fi\FamilyPortal"}
 )
 Import-Module SQLServer
 $OdsConnectStr=$EdFiParamTable.Ods
-Invoke-Sqlcmd -InputFile "C:\Ed-Fi\ParentPortal\Student1.ParentPortal.Data\Scripts\edFi31\1CreateParentPortalSupportingDatabaseSchema.sql"  -ConnectionString $OdsConnectStr
-Invoke-Sqlcmd -InputFile "C:\Ed-Fi\ParentPortal\Student1.ParentPortal.Data\Scripts\edFi31\2ODSExtensions.sql"  -ConnectionString $OdsConnectStr
-Invoke-Sqlcmd -InputFile "C:\Ed-Fi\ParentPortal\Student1.ParentPortal.Data\Scripts\edFi31\99SampleDataNeededForDemo.sql"  -ConnectionString $OdsConnectStr
+$DbScriptPath="C:\Ed-Fi\FamilyPortal\Student1.ParentPortal.Data\Scripts\edFi31"
+Invoke-Sqlcmd -InputFile "$DbScriptPath\1CreateParentPortalSupportingDatabaseSchema.sql"  -ConnectionString $OdsConnectStr
+Invoke-Sqlcmd -InputFile "$DbScriptPath\2ODSExtensions.sql"  -ConnectionString $OdsConnectStr
+Invoke-Sqlcmd -InputFile "$DbScriptPath\3StudentDetails.sql"  -ConnectionString $OdsConnectStr
+Invoke-Sqlcmd -InputFile "$DbScriptPath\4SampleDataDemo.sql"  -ConnectionString $OdsConnectStr
