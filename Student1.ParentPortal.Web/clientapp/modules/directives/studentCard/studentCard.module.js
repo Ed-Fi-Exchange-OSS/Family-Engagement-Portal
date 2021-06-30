8066b6d5a703ea4649dc8068fd43f9d5be194d10
DiracSpace <json.roberto.deleon@gmail.com>
2021-06-30 13:38:49 -0500

    Removed all color circles in HTML and CSS, fixed <i> tag bug in Student-Detail module, fixed borders in Student-Card on Teacher-Landing and added line break CSS for long e-mails

--- Student1.ParentPortal.Web/clientapp/modules/directives/studentCard/studentCard.module.js
+++ Student1.ParentPortal.Web/clientapp/modules/directives/studentCard/studentCard.module.js
@@ -36,3 +36,13 @@
             }
         }]
     });
+
+angular.module('app').filter('ifEmpty', function () {
+    return function (input, defaultValue) {
+        if (angular.isUndefined(input) || input === null || input === '') {
+            return defaultValue;
+        }
+
+        return input;
+    }
+});
